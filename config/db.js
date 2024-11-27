adbfb8061a76cb6a784aa04106b00c18e8b9bc23
GeetanshJ <geetansh0513.be21@chitkara.edu.in>
2024-11-27 18:43:06 +0530

    import error solved

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 export const connectDB = async () => {
     await mongoose.connect('mongodb+srv://jaingeetansh:geetansh@cluster0.ezxnf.mongodb.net/food-del').then(()=>{console.log("DB connected")})
