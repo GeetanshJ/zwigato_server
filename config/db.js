4813dca6be3ef2ee267cc0d08197c22028607c53
GeetanshJ <geetansh0513.be21@chitkara.edu.in>
2024-11-27 20:14:01 +0530

    solved hosting isuue

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 export const connectDB = async () => {
     await mongoose.connect('mongodb+srv://jaingeetansh:geetansh@cluster0.ezxnf.mongodb.net/food-del').then(()=>{console.log("DB connected")})
