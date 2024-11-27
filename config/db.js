ae6af6b2d841839bba104b9e28d13aee45ce507f
GeetanshJ <geetansh0513.be21@chitkara.edu.in>
2024-11-27 18:21:18 +0530

    server created

---
+++ config/db.js
@@ -0,0 +1,5 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+    await mongoose.connect('mongodb+srv://jaingeetansh:geetansh@cluster0.ezxnf.mongodb.net/food-del').then(()=>{console.log("DB connected")})
+}
