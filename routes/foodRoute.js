3a566f537aae8e694089871bd136626b5a2c9b14
GeetanshJ <geetansh0513.be21@chitkara.edu.in>
2024-11-27 18:40:27 +0530

    import error solved

--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 import { addFood, listFood,removeFood } from "../controllers/foodController.js";
 
-import multer from 'multer';
+const multer = require('multer');
 
 const foodRouter = express.Router();
 
