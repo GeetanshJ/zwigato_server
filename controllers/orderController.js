2771b1a2a2e72e423d8856360e3b848a5f73a9ca
GeetanshJ <geetansh0513.be21@chitkara.edu.in>
2024-11-29 20:00:22 +0530

    creating orderPlace

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,8 +1,26 @@
+import orderModel from "../models/orderModel.js";
 import orderSchema from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
 
 
 const placeOrder = async(req,res) =>{
+    try{
+        const newOrder = new orderModel({
+            userId:req.body.userId,
+            items:req.body.items,
+            amount:req.body.amount,
+            address:req.body.address,
+        })
+
+        await newOrder.save();
+        await userModel.findByIdAndUpdate(req.body.userId,{cartData:{}});
+
+    }
+
+    catch(Err) {
+        
+    }
+
 
 }
 
