9874baf1ad4635bae447baa47a99817f592b0f13
GeetanshJ <geetansh0513.be21@chitkara.edu.in>
2024-12-02 17:41:39 +0530

    added functionality of 1 user logged in at a time

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,72 +5,82 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const validator = require("validator");
 
+let isUserLoggedIn = false;
 
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
 
-const loginUser = async (req,res) => {
-    const{email,password} = req.body;
-
-    try{
-        const user = await userModel.findOne({email});
-
-        if(!user){
-            return res.json({success:false,message:"User does not exist"});
+    try {
+        if (isUserLoggedIn) {
+            return res.json({ success: false, message: "Only one user can be logged in at a time." });
         }
 
-        const isMatch = await bcrypt.compare(password,user.password);
+        const user = await userModel.findOne({ email });
 
-        if(!isMatch){
-            return res.json({success:false,message:"Invalid credential"});
+        if (!user) {
+            return res.json({ success: false, message: "User does not exist" });
         }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.json({ success: false, message: "Invalid credentials" });
+        }
+
+        isUserLoggedIn = true;
 
         const token = createToken(user._id);
 
-        res.json({success:true,token});
+        res.json({ success: true, token });
 
-    } catch(err){
+    } catch (err) {
         console.log(err);
-        res.json({success:false,message:"Error"});
-
+        res.json({ success: false, message: "Error" });
     }
-}
+};
 
 const createToken = (id) => {
-    return jwt.sign({id},process.env.JWT_SECRET);
-}
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30m" });
+};
 
-const registerUser = async (req,res) => {
-    const {name,password,email} = req.body;
+const registerUser = async (req, res) => {
+    const { name, password, email } = req.body;
 
-    try{
-        const exists = await userModel.findOne({email});
-        if(exists){
-            return res.json({success:false,message:"User already exists!!"});
+    try {
+        const exists = await userModel.findOne({ email });
+        if (exists) {
+            return res.json({ success: false, message: "User already exists!!" });
         }
 
-        if(!validator.isEmail(email)){
-            return res.json({success:false,message:"Enter valid email!!"});
+        if (!validator.isEmail(email)) {
+            return res.json({ success: false, message: "Enter a valid email!!" });
         }
 
-        if(password.length < 8){
-            return res.json({success:false,message:"Password length min 8 characters!!"});
-
+        if (password.length < 8) {
+            return res.json({ success: false, message: "Password length must be at least 8 characters!!" });
         }
 
         const salt = await bcrypt.genSalt(10);
-        const bcryptPass = await bcrypt.hash(password,salt);
+        const bcryptPass = await bcrypt.hash(password, salt);
 
-        const newUser = new userModel({name:name,email:email,password:bcryptPass});
+        const newUser = new userModel({ name: name, email: email, password: bcryptPass });
         const user = await newUser.save();
 
-        const token = createToken(user._id)
-        res.json({success:true,token});
-    }
-    
-    catch(err){
+        const token = createToken(user._id);
+        res.json({ success: true, token });
+    } catch (err) {
         console.log(err);
-        res.json({success:false,message:"Error"});
+        res.json({ success: false, message: "Error" });
     }
-}
+};
 
-export {loginUser,registerUser};
+const logoutUser = (req, res) => {
+    if (!isUserLoggedIn) {
+        return res.json({ success: false, message: "No user is currently logged in." });
+    }
 
+    isUserLoggedIn = false;
+    res.json({ success: true, message: "Logged out successfully" });
+};
+
+export { loginUser, registerUser, logoutUser };
