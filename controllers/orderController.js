6ca9e2cbbd896d7e2f9522dc44cdaff6f16f1d72
GeetanshJ <geetansh0513.be21@chitkara.edu.in>
2024-12-03 20:16:41 +0530

    solving stripe problem

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -3,7 +3,7 @@ import orderSchema from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
 import Stripe from "stripe";
 
-const stipe = new Stripe(process.env.STRIPE_SECRET_KEY)
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 
 const placeOrder = async(req,res) =>{
